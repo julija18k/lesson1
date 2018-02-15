ee2add8ce9662641219382c314852b30a71fd97e
julija <julija18k@yandex.ru>
2018-02-15 20:58:38 +0300

    initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args){
+       int a=5, b=10;
+       int c= (a*b+2)*3;
+       System.out.println(c);
+       float d = 20.f/15.5f;
+       System.out.println(d);
+
+
+    }
+}
